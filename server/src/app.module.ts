702b5f4f6fb40367a1fd898f2fdcc3b46526dbb1
adbarc92 <adbarclay92@gmail.com>
2023-01-03 13:38:20 -0700

    WIP; adds MySQL config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { ShiftItemsModule } from 'src/models/shiftItems/shiftItems.module';
 import { ShiftsModule } from 'src/models/shifts/shifts.module';
+import TypeOrmConfig from 'src/config/database/mysql/configuration';
 
 @Module({
-  imports: [ShiftsModule, ShiftItemsModule],
+  imports: [TypeOrmConfig, ShiftsModule, ShiftItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
